53ca406f444399ea54ca60929aa1f5b46c51d486
tobymanders <Ao43KzE>
2019-02-25 11:04:32 -0600

    Remove .idea from repo

--- rftg/power.py
+++
@@ -1,6 +0,0 @@
-
-def consume_any_color(consume_limit, vp_per_resource, card_per_resource, deck, tableau):
-    for i in range(0, consume_limit):
-        if tableau.any_goods():
-            [index, kind] = tableau.locate_goods()
-            tableau.burn_good(index)
